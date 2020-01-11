6b756f7b65aaf063f0fb3a2e8d4f200bf49e8404
fuchina <1245102331@qq.com>
2020-01-11 21:39:20 +0800

    方法优化

--- FSUIKit/Classes/UIView+ModalAnimation.h
+++ FSUIKit/Classes/UIView+ModalAnimation.h
@@ -12,7 +12,14 @@ NS_ASSUME_NONNULL_BEGIN
 @interface UIView (ModalAnimation)
 
 - (void)pushAnimated:(BOOL)flag completion:(void (^ __nullable)(UIView *modalView))completion;
-- (void)popAnimated:(BOOL)flag completion:(void (^ __nullable)(UIView *modalView))completion;
+- (void)pushAnimated:(BOOL)flag toFrame:(CGRect)frame completion:(void (^ __nullable)(UIView *modalView))completion;
+
+/**
+ * remove是否从父视图移除，如果不移除，对象会不释放
+ */
+- (void)popAnimated:(BOOL)flag removeFromSuperView:(BOOL)remove completion:(void (^ __nullable)(UIView *modalView))completion;
+
+- (void)popAnimated:(BOOL)flag toFrame:(CGRect)frame removeFromSuperView:(BOOL)remove completion:(void (^ __nullable)(UIView *modalView))completion;
 
 @end
 
--- FSUIKit/Classes/UIView+ModalAnimation.m
+++ FSUIKit/Classes/UIView+ModalAnimation.m
@@ -10,49 +10,62 @@
 @implementation UIView (ModalAnimation)
 
 - (void)pushAnimated:(BOOL)flag completion:(void (^ __nullable)(UIView *modalView))completion{
-    if (flag) {
-        self.frame = CGRectMake(0, UIScreen.mainScreen.bounds.size.height, UIScreen.mainScreen.bounds.size.width, UIScreen.mainScreen.bounds.size.height);
-        [UIView animateWithDuration:.5
-                              delay:0
-             usingSpringWithDamping:1 /*震动效果，0-1，越小越明显*/
-              initialSpringVelocity:0.5
-                            options:UIViewAnimationOptionCurveEaseOut
-                         animations:^{
-                             self.frame = CGRectMake(0, 0, UIScreen.mainScreen.bounds.size.width, UIScreen.mainScreen.bounds.size.height);
-                         }
-                         completion:^(BOOL finished) {
-                             if (completion) {
-                                 completion(self);
-                             }
-                         }];
-    }else{
-        self.frame = CGRectMake(0, 0, UIScreen.mainScreen.bounds.size.width, UIScreen.mainScreen.bounds.size.height);
-        if (completion) {
-            completion(self);
-        }
-    }
+    [self pushAnimated:flag toFrame:CGRectMake(0, 0, UIScreen.mainScreen.bounds.size.width, UIScreen.mainScreen.bounds.size.height) completion:completion];
 }
 
-- (void)popAnimated:(BOOL)flag completion:(void (^ __nullable)(UIView *modalView))completion{
+- (void)pushAnimated:(BOOL)flag toFrame:(CGRect)frame completion:(void (^ __nullable)(UIView *modalView))completion {
     if (flag) {
-        [UIView animateWithDuration:.5
-                              delay:0
-             usingSpringWithDamping:1 /*震动效果，0-1，越小越明显*/
-              initialSpringVelocity:0.5
-                            options:UIViewAnimationOptionCurveEaseOut
-                         animations:^{
-                             self.frame = CGRectMake(0, UIScreen.mainScreen.bounds.size.height, UIScreen.mainScreen.bounds.size.width, UIScreen.mainScreen.bounds.size.height);
-                         }
-                         completion:^(BOOL finished) {
-                             if (completion) {
-                                 completion(self);
-                             }
-                         }];
-    }else{
-        self.frame = CGRectMake(0, UIScreen.mainScreen.bounds.size.height, UIScreen.mainScreen.bounds.size.width, UIScreen.mainScreen.bounds.size.height);
-        if (completion) {
-            completion(self);
-        }
+           self.frame = CGRectMake(0, UIScreen.mainScreen.bounds.size.height, UIScreen.mainScreen.bounds.size.width, UIScreen.mainScreen.bounds.size.height);
+           [UIView animateWithDuration:.5
+                                 delay:0
+                usingSpringWithDamping:1 /*震动效果，0-1，越小越明显*/
+                 initialSpringVelocity:0.5
+                               options:UIViewAnimationOptionCurveEaseOut
+                            animations:^{
+                                self.frame = frame;
+                            }
+                            completion:^(BOOL finished) {
+                                if (completion) {
+                                    completion(self);
+                                }
+                            }];
+       }else{
+           self.frame = frame;
+           if (completion) {
+               completion(self);
+           }
+       }
+}
+
+- (void)popAnimated:(BOOL)flag removeFromSuperView:(BOOL)remove completion:(void (^ __nullable)(UIView *modalView))completion {
+    [self popAnimated:flag toFrame:CGRectMake(0, UIScreen.mainScreen.bounds.size.height, UIScreen.mainScreen.bounds.size.width, UIScreen.mainScreen.bounds.size.height) removeFromSuperView:remove completion:completion];
+}
+
+- (void)popAnimated:(BOOL)flag toFrame:(CGRect)frame removeFromSuperView:(BOOL)remove completion:(void (^ __nullable)(UIView *modalView))completion {
+    if (flag) {
+           [UIView animateWithDuration:.5
+                                 delay:0
+                usingSpringWithDamping:1 /*震动效果，0-1，越小越明显*/
+                 initialSpringVelocity:0.5
+                               options:UIViewAnimationOptionCurveEaseOut
+                            animations:^{
+                                self.frame = frame;
+                            }
+                            completion:^(BOOL finished) {
+                              [self popCallback:remove completion:completion];
+                            }];
+       }else{
+           self.frame = frame;
+           [self popCallback:remove completion:completion];
+       }
+}
+
+- (void)popCallback:(BOOL)remove completion:(void (^ __nullable)(UIView *modalView))completion {
+    if (completion) {
+        completion(self);
+    }
+    if (remove) {
+        [self removeFromSuperview];
     }
 }
 
