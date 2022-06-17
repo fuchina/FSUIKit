0fe82a010f9ffe2e88d75178aa138fdc32a77a7a
fudongdong <fudongdong@ihuman.com>
2022-06-17 17:50:01 +0800

    优化

--- FSUIKit/Classes/FSImageView.m
+++ FSUIKit/Classes/FSImageView.m
@@ -7,9 +7,11 @@
 
 #import "FSImageView.h"
 
-@implementation FSImageView
+@implementation FSImageView {
+    UIView      *_back_tap_view;
+}
 
-- (instancetype)initWithFrame:(CGRect)frame{
+- (instancetype)initWithFrame:(CGRect)frame {
     self = [super initWithFrame:frame];
     if (self) {
         [self fs_add_tap_event_in_base_view];
@@ -17,15 +19,23 @@
     return self;
 }
 
-- (void)fs_add_tap_event_in_base_view{
+- (void)fs_add_tap_event_in_base_view {
+    _back_tap_view = [[UIView alloc] initWithFrame:self.bounds];
+    [self addSubview:_back_tap_view];
+    
     UITapGestureRecognizer *one = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(_fs_tap_click_event:)];
-    [self addGestureRecognizer:one];
+    [_back_tap_view addGestureRecognizer:one];
 }
 
 - (void)_fs_tap_click_event:(UITapGestureRecognizer *)tap {
     if (self.click) {
         self.click(self);
     }
+}
+
+- (void)layoutSubviews {
+    [super layoutSubviews];
+    _back_tap_view.frame = self.bounds;
 }
 
 
