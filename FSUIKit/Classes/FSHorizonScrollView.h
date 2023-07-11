7655f802704f6e144169e97a5e98d07c79a94316
fudongdong <fudongdong@ihuman.com>
2023-07-11 10:12:15 +0800

    #import "FSHorizonScrollView.h"

---
+++ FSUIKit/Classes/FSHorizonScrollView.h
@@ -0,0 +1,16 @@
+//
+//  FSHorizonScrollView.h
+//  FSUIKit
+//
+//  Created by 扶冬冬 on 2023/7/11.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FSHorizonScrollView : UIScrollView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ FSUIKit/Classes/FSHorizonScrollView.m
@@ -0,0 +1,49 @@
+//
+//  FSHorizonScrollView.m
+//  FSUIKit
+//
+//  Created by 扶冬冬 on 2023/7/11.
+//
+
+#import "FSHorizonScrollView.h"
+
+@implementation FSHorizonScrollView
+
+- (BOOL)gestureRecognizerShouldBegin:(UIGestureRecognizer *)gestureRecognizer {
+    if ([self panBack:gestureRecognizer]) {
+        return NO;
+    }
+    return YES;
+}
+
+- (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldRecognizeSimultaneouslyWithGestureRecognizer:(nonnull UIGestureRecognizer *)otherGestureRecognizer {
+    if ([self panBack:gestureRecognizer]) {
+        return YES;
+    }
+    return NO;
+}
+
+- (BOOL)panBack:(UIGestureRecognizer *)gestureRecognizer {
+    if (gestureRecognizer == self.panGestureRecognizer) {
+        UIPanGestureRecognizer *pan = (UIPanGestureRecognizer *)gestureRecognizer;
+        UIGestureRecognizerState state = gestureRecognizer.state;
+        if (UIGestureRecognizerStateBegan == state || UIGestureRecognizerStatePossible == state) {
+            CGPoint point = [pan translationInView:self];
+            CGPoint location = [gestureRecognizer locationInView:self];
+            if (point.x > 0 && location.x < 90 && self.contentOffset.x <= 0) {
+                return YES;
+            }
+        }
+    }
+    return NO;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
