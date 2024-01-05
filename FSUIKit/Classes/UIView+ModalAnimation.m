8015ca9af85046dc1d5888399f011742727d66ad
fudongdong <fudongdong@ihuman.com>
2024-01-05 10:25:02 +0800

    Update UIView+ModalAnimation.m

--- FSUIKit/Classes/UIView+ModalAnimation.m
+++ FSUIKit/Classes/UIView+ModalAnimation.m
@@ -9,32 +9,28 @@
 
 @implementation UIView (ModalAnimation)
 
-- (void)pushAnimated:(BOOL)flag completion:(void (^ __nullable)(UIView *modalView))completion{
+- (void)pushAnimated:(BOOL)flag completion:(void (^ __nullable)(UIView *modalView))completion {
     [self pushAnimated:flag toFrame:CGRectMake(0, 0, UIScreen.mainScreen.bounds.size.width, UIScreen.mainScreen.bounds.size.height) completion:completion];
 }
 
 - (void)pushAnimated:(BOOL)flag toFrame:(CGRect)frame completion:(void (^ __nullable)(UIView *modalView))completion {
     if (flag) {
            self.frame = CGRectMake(0, UIScreen.mainScreen.bounds.size.height, UIScreen.mainScreen.bounds.size.width, UIScreen.mainScreen.bounds.size.height);
-           [UIView animateWithDuration:.5
-                                 delay:0
-                usingSpringWithDamping:1 /*震动效果，0-1，越小越明显*/
-                 initialSpringVelocity:0.5
+           [UIView animateWithDuration:.5 delay:0 usingSpringWithDamping:1 /*震动效果，0-1，越小越明显*/ initialSpringVelocity:0.5
                                options:UIViewAnimationOptionCurveEaseOut
                             animations:^{
                                 self.frame = frame;
-                            }
-                            completion:^(BOOL finished) {
+                            } completion:^(BOOL finished) {
                                 if (completion) {
                                     completion(self);
                                 }
                             }];
-       }else{
-           self.frame = frame;
-           if (completion) {
-               completion(self);
-           }
-       }
+    } else {
+        self.frame = frame;
+        if (completion) {
+            completion(self);
+        }
+    }
 }
 
 - (void)popAnimated:(BOOL)flag removeFromSuperView:(BOOL)remove completion:(void (^ __nullable)(UIView *modalView))completion {
@@ -54,7 +50,7 @@
                             completion:^(BOOL finished) {
                               [self popCallback:remove completion:completion];
                             }];
-       }else{
+       } else {
            self.frame = frame;
            [self popCallback:remove completion:completion];
        }
@@ -66,6 +62,8 @@
     }
     
     if (remove) {
+        self.frame = CGRectMake(10000, 0, 0, 0);
+        self.hidden = YES;
         [self removeFromSuperview];
     }
 }
