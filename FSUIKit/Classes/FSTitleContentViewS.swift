fa4ba50b50ffd830b52843d273b65ebf4c9e7323
fuchina <1245102331@qq.com>
2026-01-29 22:56:32 +0800

    Update FSTitleContentViewS.swift

--- FSUIKit/Classes/FSTitleContentViewS.swift
+++ FSUIKit/Classes/FSTitleContentViewS.swift
@@ -7,4 +7,19 @@
 
 import Foundation
 
-class FSTitleContentViewS
+open class FSTitleContentViewS: FSViewS {
+    
+    public lazy var label: UILabel = {
+        let l = UILabel(frame: bounds)
+        self.addSubview(l)
+        return l
+    }()
+    
+    public lazy var contentLabel: UILabel = {
+        let l = UILabel(frame: bounds)
+        l.textAlignment = .right
+        self.addSubview(l)
+        return l
+    }()
+    
+}
